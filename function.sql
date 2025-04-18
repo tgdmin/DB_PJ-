cdcbada078c7c5cdd72e596ecbe14f565befcb44
tgdmin <127995941+tgdmin@users.noreply.github.com>
2025-04-18 12:38:54 +0300

    Update function.sql

--- function.sql
+++ function.sql
@@ -7,3 +7,30 @@ FROM book_authors ba
 JOIN authors a ON ba.author_id = a.id
 WHERE a.name = author_name;
 $$;
+
+CREATE OR REPLACE FUNCTION calculate_author_commissions()
+RETURNS TABLE (
+    author_id INT,
+    author_name VARCHAR(100),
+    total_sales NUMERIC(10,2),
+    total_commission NUMERIC(10,2)
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT 
+        a.id AS author_id,
+        a.name AS author_name,
+        SUM(oi.quantity * oi.price_at_purchase) AS total_sales,
+        ROUND(SUM(oi.quantity * oi.price_at_purchase) * 0.1, 2) AS total_commission
+    FROM 
+        authors a
+    JOIN 
+        book_authors ba ON a.id = ba.author_id
+    JOIN 
+        order_items oi ON ba.book_id = oi.book_id
+    GROUP BY 
+        a.id, a.name
+    ORDER BY 
+        total_commission DESC;
+END;
+$$ LANGUAGE plpgsql;
